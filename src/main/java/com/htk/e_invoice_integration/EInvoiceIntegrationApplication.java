13290a6111c79f58913d79662c44d5d469726359
Harun Taha Kepenek <hakukpnk@gmail.com>
2025-06-09 01:31:56 +0300

    initial commit

---
+++ src/main/java/com/htk/e_invoice_integration/EInvoiceIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.htk.e_invoice_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EInvoiceIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EInvoiceIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/htk/e_invoice_integration/EInvoiceIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htk.e_invoice_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EInvoiceIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
